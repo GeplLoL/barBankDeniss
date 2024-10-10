9d09bd9fc6f3799af6fed16e882d2ff29f5e0b9c
GeplLoL <goryunov.deniska@list.ru>
2024-10-10 13:02:16 +0300

    valmis

--- src/routes/register.svelte
+++ src/routes/register.svelte
@@ -15,8 +15,7 @@
         // TODO handle network error
         error = response.error;
 
-        if (response.user) {
-            $session.user = response.user;
+        if (response.id) {
             goto('/');
         }
     }
@@ -30,14 +29,14 @@
     <div class="container page">
         <div class="row">
             <div class="col-md-6 offset-md-3 col-xs-12">
-                <h1 class="text-xs-center">Sign up</h1>
+                <h1 class="text-xs-center">Sign up * barBank</h1>
                 <p class="text-xs-center">
                     <a href="/login">Have an account?</a>
                 </p>
                 {#if error}
-                    {error}
+                    <div class="alert alert-danger" role="alert">{error}</div>
                 {/if}
-                    <form on:submit|preventDefault={submit}>
+                <form on:submit|preventDefault={submit}>
                     <fieldset class="form-group">
                         <input class="form-control form-control-lg" type="text" required placeholder="Your username" bind:value={username}>
                     </fieldset>
@@ -46,8 +45,11 @@
                     </fieldset>
                     <fieldset class="form-group">
                         <input class="form-control form-control-lg" type="password" required placeholder="Password" bind:value={password}>
+                        {#if password.length > 1 && password.length < 6}
+                            <sup><div class="alert alert-danger" role="alert">Password too short</div></sup>
+                        {/if}
                     </fieldset>
-                    <button class="btn btn-lg btn-primary pull-xs-right">
+                    <button class="btn btn-lg btn-primary pull-xs-right" disabled="{password.length < 6}">
                         Sign up
                     </button>
                 </form>
